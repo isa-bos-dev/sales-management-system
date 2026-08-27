0f55a1bc59046b8d681d70836158dc3c98fa76d3
IsabBosDev <isabosdev@gmail.com>
2026-08-27 12:15:11 +0100

    feat(backend): add sales DTOs and generic ApiResponse wrapper

---
+++ Backend/SalesWebApi/DTOs/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace SalesWebApi.DTOs { 
+
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> Success(T data, string message = "")
+        {
+            return new ApiResponse<T>
+            {
+                IsSuccess = true,
+                Data = data,
+                Message = message
+               
+            };
+        }
+
+        public static ApiResponse<T> Failure(string message)
+        {
+            return new ApiResponse<T>
+            {
+                IsSuccess = false,
+                Message = message,
+            };
+        }
+    }
+}
---
+++ Backend/SalesWebApi/DTOs/SaleDtos.cs
@@ -0,0 +1,44 @@
+﻿using SalesWebApi.Data.Enums;
+
+namespace SalesWebApi.DTOs {
+
+    #region requests
+    public record CreateSaleRequest(
+        string CustomerName,
+        int PaymentTypeValue,
+        decimal Total,
+        IEnumerable<CreateSaleDetailRequest> Details);
+
+    public record CreateSaleDetailRequest(
+        string ProductName,
+        int Quantity,
+        decimal UnitPrice);
+
+    public record GetSalesQueryRequest(
+        int Page,
+        int PageSize);
+
+    #endregion
+
+    #region Responses
+    public record GetSaleResponse(
+        int SaleId,
+        string CustomerName,
+        string PaymentTypeName,
+        decimal Total,
+        string SaleDate,
+        IEnumerable<GetSaleDetailResponse>? Details = null);
+
+    public record GetSaleDetailResponse(
+        string ProductName,
+        int Quantity,
+        decimal UnitPrice);
+
+    public record GetSalesQueryResponse(
+        IEnumerable<GetSaleResponse> Items,
+        int Page,
+        int PageSize,
+        int TotalItems);
+
+        #endregion
+}
