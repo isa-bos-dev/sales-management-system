97e97d4544a5f5f645cfc1d64ab466ff14f48544
IsabBosDev <isabosdev@gmail.com>
2026-08-27 18:53:00 +0100

    feat(frontend): configure routing and main router-outlet layout

--- Frontend/sale-app/src/app/app.routes.ts
+++ Frontend/sale-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ListSalesPage } from './pages/list-sales-page/list-sales-page';
+import { NewSalePage } from './pages/new-sale-page/new-sale-page';
+import { DetailSalePage } from './pages/detail-sale-page/detail-sale-page';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    // Default route: load sales list component
+  { path: '', component:ListSalesPage},
+
+  // Route to create a new sale
+  { path: 'new', component: NewSalePage },
+
+  // Route to display single sale details by ID parameter
+  { path: 'detail/:id', component: DetailSalePage }
+];
